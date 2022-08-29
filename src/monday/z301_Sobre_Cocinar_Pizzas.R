67301199466577fce4fd1d7c74c875c470c4043b
federicochecozzi <tiamantex@hotmail.com>
2022-08-29 19:08:33 -0300

    Experimentos con optimización bayesiana realizados

--- src/monday/z301_Sobre_Cocinar_Pizzas.R
+++ src/monday/z301_Sobre_Cocinar_Pizzas.R
@@ -24,9 +24,9 @@ require("DiceKriging")
 require("mlrMBO")
 
 # Poner la carpeta de la materia de SU computadora local
-setwd("/home/aleb/dmeyf2022")
+setwd("C:\\Users\\tiama\\OneDrive\\Documentos\\DMEyF_2022")
 # Poner sus semillas
-semillas <- c(17, 19, 23, 29, 31)
+semillas <- c(671017, 273107, 827251, 967693, 247591)
 
 # Cargamos el dataset
 dataset <- fread("./datasets/competencia1_2022.csv")
@@ -172,8 +172,10 @@ table(dataset[ds_sample]$clase_binaria)
 ## - ¿Hay mejores formas de muestrear?
 ## - ¿Es bueno muestrear?
 ## - ¿Qué efectos en las métricas va a producir el muestreo?
-## - ¿Por qué se eligió usar el AUC?
-## - ¿Qué hay que cambiar en la función de ganancia para poder utilizarla?
+## - ¿Por qué se eligió usar el AUC? 
+## No se ve afectada por el desbalanceo
+## - ¿Qué hay que cambiar en la función de ganancia para poder utilizarla? 
+## Cambiar los coeficientes de ganancia para ajustar por proporción
 
 ## ---------------------------
 ## Step 6: Comparando tiempos con o sin muestras
@@ -384,7 +386,118 @@ surr_km <- makeLearner("regr.km", predict.type = "se", covtype = "matern3_2")
 run_md_ms <- mbo(obj_fun, learner = surr_km, control = ctrl, )
 print(run_md_ms)
 
+## ---------------------------
+## Step 11: Buscando con una Opt. Bayesiana para 3 parámetros
+## ---------------------------
+
+set.seed(semillas[1])
+obj_fun_md_ms_mb <- function(x) {
+  experimento_rpart(dataset, semillas
+                    , md = x$maxdepth
+                    , ms = x$minsplit
+                    , mb = floor(x$minbucket * x$minsplit))
+}
+
+obj_fun <- makeSingleObjectiveFunction(
+  minimize = FALSE,
+  fn = obj_fun_md_ms_mb,
+  par.set = makeParamSet(
+    makeIntegerParam("maxdepth",  lower = 4L, upper = 20L),
+    makeIntegerParam("minsplit",  lower = 1L, upper = 200L),
+    makeNumericParam("minbucket", lower = 0L, upper = 1L)
+  ),
+  # noisy = TRUE,
+  has.simple.signature = FALSE
+)
+
+ctrl <- makeMBOControl()
+ctrl <- setMBOControlTermination(ctrl, iters = 30L)
+ctrl <- setMBOControlInfill(
+  ctrl,
+  crit = makeMBOInfillCritEI(),
+  opt = "focussearch",
+  # sacar parámetro opt.focussearch.points en próximas ejecuciones
+  opt.focussearch.points = 20
+)
+
+lrn <- makeMBOLearner(ctrl, obj_fun)
+
+surr_km <- makeLearner("regr.km", predict.type = "se", covtype = "matern3_2")
+
+run_md_ms_mb <- mbo(obj_fun, learner = surr_km, control = ctrl, )
+print(run_md_ms_mb)
+
 ## TAREA:
 ## Agregue todos los parámetros que considere. Una vez que tenga sus mejores
 ## parámetros, haga una copia del script rpart/z101_PrimerModelo.R, cambie los
 ## parámetros dentro del script, ejecutelo y suba a Kaggle su modelo.
+
+## Nota: Modificar experimento_rpart para no muestrear y usar ganancia en vez de auc
+
+set.seed(semillas[1])
+
+modelo_rpart2 <- function(train, test, cp =  0, ms = 20, mb = 1, md = 10) {
+  modelo <- rpart(clase_binaria ~ ., data = train,
+                  xval = 0,
+                  cp = cp,
+                  minsplit = ms,
+                  minbucket = mb,
+                  maxdepth = md)
+  
+  test_prediccion <- predict(modelo, test, type = "prob")
+  
+  ganancia(test_prediccion[, "evento"], test$clase_binaria) / 0.3
+}
+
+experimento_rpart2 <- function(ds, semillas, cp = 0, ms = 20, mb = 1, md = 10) {
+  gain <- c()
+  for (s in semillas) {
+    set.seed(s)
+    in_training <- caret::createDataPartition(ds$clase_binaria, p = 0.70,
+                                              list = FALSE)
+    train  <-  ds[in_training, ]
+    test   <-  ds[-in_training, ]
+    r <- modelo_rpart2(train, test, 
+                      cp = cp, ms = ms, mb = mb, md = md)
+    gain <- c(gain, r)
+  }
+  mean(gain)
+}
+
+obj_fun_md_ms_mb_cp <- function(x) {
+  experimento_rpart2(dataset, semillas
+                    , md = x$maxdepth
+                    , ms = x$minsplit
+                    , mb = floor(x$minbucket * x$minsplit)
+                    , cp = x$cp)
+}
+
+obj_fun <- makeSingleObjectiveFunction(
+  minimize = FALSE,
+  fn = obj_fun_md_ms_mb_cp,
+  par.set = makeParamSet(
+    makeIntegerParam("maxdepth",  lower = 4L, upper = 20L),
+    makeIntegerParam("minsplit",  lower = 1L, upper = 200L),
+    makeNumericParam("minbucket", lower = 0L, upper = 1L),
+    makeNumericParam("cp", lower = -0.1, upper = 0.1)
+  ),
+  # noisy = TRUE,
+  has.simple.signature = FALSE
+)
+
+ctrl <- makeMBOControl()
+ctrl <- setMBOControlTermination(ctrl, iters = 200L)
+ctrl <- setMBOControlInfill(
+  ctrl,
+  crit = makeMBOInfillCritEI(),
+  opt = "focussearch",
+  # sacar parámetro opt.focussearch.points en próximas ejecuciones
+  opt.focussearch.points = 20
+)
+
+lrn <- makeMBOLearner(ctrl, obj_fun)
+
+surr_km <- makeLearner("regr.km", predict.type = "se", covtype = "matern3_2")
+
+run_md_ms_mb_cp <- mbo(obj_fun, learner = surr_km, control = ctrl, )
+print(run_md_ms_mb_cp)
