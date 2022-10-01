04785b29f82e82a9740782af68be5d6f89c73674
federicochecozzi <tiamantex@hotmail.com>
2022-10-01 14:32:41 -0300

    más cambios menores

--- src/monday/z801_Sobre_mas_features.R
+++ src/monday/z801_Sobre_mas_features.R
@@ -17,14 +17,46 @@ require("rpart")
 require("ggplot2")
 require("lightgbm")
 require("xgboost")
+require("dplyr")
 
 # Poner la carpeta de la materia de SU computadora local
-setwd("/home/aleb/dmeyf2022")
+setwd("C:\\Users\\tiama\\OneDrive\\Documentos\\DMEyF_2022")
 # Poner sus semillas
-semillas <- c(17, 19, 23, 29, 31)
+semillas <- c(671017, 273107, 827251, 967693, 247591)
 
 # Cargamos los datasets y nos quedamos solo con 202101 y 202103
 dataset <- fread("./datasets/competencia2_2022.csv.gz")
+#dataset <- fread("./exp/FE777002b/dataset_con_features.csv")
+
+# mis_variables <- c("ctrx_quarter",
+#                    "mprestamos_personales",
+#                    "mcuentas_saldo",
+#                    "mactivos_margen",
+#                    "mcaja_ahorro",
+#                    "mcuenta_corriente")
+# 
+# prefix <- "r_"
+# for (var in mis_variables) {
+#   dataset[, (paste(prefix, var, sep = "")) := ntile(get(var), 10)]#get permite obtener la variable a partir de su nombre
+# }
+# 
+# dataset[, cociente_mrentabilidad := mrentabilidad_annual/(mrentabilidad + 1)] #sin cambios debería ser alrededor de 12
+# 
+# dataset[, cuentas_totales := rowSums(.SD, na.rm = TRUE), .SDcols = c("ccuenta_corriente", "ccaja_ahorro")]
+# 
+# dataset[, mprestamos := rowSums(.SD, na.rm = TRUE), .SDcols = c("mprestamos_personales", "mprestamos_prendarios", "mprestamos_hipotecarios")]
+# 
+# dataset[, cociente_mplazo_fijo := mplazo_fijo_dolares/(mplazo_fijo_pesos+ 1)]
+# 
+# dataset[, suma_mplazo_fijo := rowSums(.SD, na.rm = TRUE), .SDcols = c("mplazo_fijo_dolares", "mplazo_fijo_pesos")]
+# 
+# dataset[, suma_minversion := rowSums(.SD, na.rm = TRUE), .SDcols = c("minversion1_pesos", "minversion2")]
+# 
+# dataset[, cseguro := rowSums(.SD, na.rm = TRUE), .SDcols = c("cseguro_vida", "cseguro_auto", "cseguro_vivienda","cseguro_accidentes_personales")]
+# 
+# dataset[, suma_mpayroll := rowSums(.SD, na.rm = TRUE), .SDcols = c("mpayroll", "mpayroll2")]
+
+
 marzo <- dataset[foto_mes == 202103]
 mayo <- dataset[foto_mes == 202105]
 rm(dataset)
@@ -96,7 +128,7 @@ param_fe2 <- list(
                 colsample_bynode = 0.8,
                 learning_rate = 1,
                 max_depth = 3, # <--- IMPORTANTE CAMBIAR
-                num_parallel_tree = 10, # <--- IMPORTANTE CAMBIAR
+                num_parallel_tree = 10, # <--- IMPORTANTE CAMBIAR 
                 subsample = 0.8,
                 objective = "binary:logistic"
             )
